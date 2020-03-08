42261a8213113987d40ece81f09ae674e34029bf
Jared Fleming <jaredmf10@gmail.com>
2020-03-08 23:35:23 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 // Write your solution in this file!
 let driver = {}
 
-function destructivelyDeleteFromDriverByKey(driver, key) {
+function destructivelyDeleteFromDriverByKey(driver, key, value) {
 
 }
